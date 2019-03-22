ce1d05a452f8334ff8d4fe0066c91ff027ea1f45
Robin Paitry <paitrydev@gmail.com>
2019-03-22 09:19:44 +0100

    Add files via upload

---
+++ padme.php
@@ -0,0 +1,128 @@
+<?php
+include('header.php')
+?>
+
+<body>
+
+
+
+    <!--PAGE 1 Padme-->
+    <div class="container-fluid padme">
+
+        <div class="container  ">
+            <div class="row">
+                <div class=" col-3 col-sm-3 col-lg-3 ">
+                    <a href="index.php"><img src="img/senat.png" class="mt-5 pl-5" alt=""></a>
+
+                </div>
+                <div class=" col-3 col-sm-3 col-lg-3 ">
+                    <a href="Anakin.php"><img src="img/anakin.png" class="mt-5 pl-5" alt=""></a>
+
+                </div>
+                <div class=" col-3 col-sm-3 col-lg-3 ">
+                    <a href="Leia_Luke.php"><img src="img/luke-leia.png" class="mt-5 pl-5" alt=""></a>
+
+                </div>
+                <div class=" col-3 col-sm-3 col-lg-3 ">
+                    <a href="contact.php"><img src="img/etoile-noire.png" class="mt-5 pl-5" alt=""></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-sm-12 col-lg-12 text-white mt-5">
+                    <h3>Padmé Amidala</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class=" col-12 col-sm-4 col-lg-6 p-5 ">
+                    <img class="mt-2 img-fluid image" src="img/padme-profil.jpg" alt="Queen Amidala">
+                </div>
+
+                <div class="col-12 col-sm-8 col-lg-6 text-white p-5">
+                    <h2>L'histoire d'une grande dame </h2>
+                    <p class="pt-3">Padmé Amidala Skywalker est née en 46 av. BY sur Naboo et morte en 19 av. BY sur
+                        Polis Massa. Elle est élue reine de Naboo en 33 av. BY et effectue deux mandats de 4 ans à ce
+                        poste. Nommée par la reine Jamillia, elle devient sénatrice de Naboo au Sénat galactique en 24
+                        av. BY après son 2e mandat. Amoureuse du chevalier Jedi Anakin Skywalker, elle l'épouse en
+                        secret et donne naissance aux jumeaux Luke Skywalker et Leia Organa avant de mourir, ayant
+                        perdu l'amour de sa vie au moment où ce dernier bascule du côté obscur de la Force et devient
+                        Dark Vador, en gardant l'espoir qu'il y ait encore du bon en lui.<br>
+                        Durant l'épisode I (32 av. BY), elle mène la Bataille de Naboo et met en œuvre une politique de
+                        réconciliation avec les Gungans. Dans l'épisode II (22 av. BY) elle mène l'opposition au Sénat
+                        à la création d'une armée de la République. Enfin dans l'épisode III (19 av. BY), elle fait
+                        partie de la délégation des 2000 exigeant l'abandon des pouvoirs exceptionnels du Chancelier
+                        Palpatine.
+                    </p>
+                </div>
+            </div>
+       
+
+
+
+        <div class="row">
+            <div class="col-12 col-sm-4 col-lg-6 text-white p-5">
+                <h2>Sa jeunesse</h2>
+                <p class="pt-3">Padmé Naberrie est née dans un petit village de montagne non loin de la région des
+                    lacs sur Naboo. Sa mère, Jobal, et son père, Ruwee, ont également une autre fille, Sola, qui a
+                    elle-même deux enfants, Ryoo et Pooja. Très tôt, Padmé s’engage activement pour le Mouvement
+                    d’Accueil des Réfugiés qui porte assistance aux immigrants souhaitant intégrer la planète. À
+                    huit ans, elle entre à l’école des Apprentis Législateurs. Ses capacités étonnantes et sa force
+                    de caractère lui ouvrent l’accès au titre d’Apprentie à l’âge de onze ans. Dévouée à la cause
+                    publique, son acharnement force le respect de ses pairs qui la nomment Gouverneur de Theed,
+                    capitale de Naboo. Elle vient d’avoir douze ans. Suite à l’abdication du Roi Veruna, elle est
+                    élue Reine à la majorité absolue à seulement quatorze ans. Elle prend alors son nom de fonction
+                    : Amidala. Peu de temps après son arrivée au pouvoir, elle doit faire face à l'invasion
+                    illégale de la Fédération du commerce.
+                </p>
+            </div>
+
+            <div class="col-12 col-sm-4 col-lg-6 p-5 text-white">
+                <h2>Sa famille </h2>
+                <p class="pt-3">La famille Naberrie regroupe plusieurs personnages de la saga Star Wars :
+
+                    Jobal Naberrie est la fille de Ryoo Thule, la femme de Ruwee Naberrie, la mère de Padmé Amidala
+                    et de Sola Naberrie, et la grand-mère de Ryoo Naberrie, Pooja Naberrie, Luke Skywalker et Leia
+                    Organa. Elle apparaît dans une scène coupée de Star Wars, épisode II : L'Attaque des clones.
+                    Jobal apparaît avec le reste de la famille dans Star Wars, épisode III : La Revanche des Sith
+                    pendant la marche funéraire de Padmé. Jobal Naberrie est jouée au cinéma par Trisha Noble.
+                    Sola Naberrie est la fille de Jobal Naberrie, la sœur de Padmé Amidala. Elle aura deux enfants
+                    : Pooja Naberrie et Ryoo Naberrie.
+                    Ryoo Naberrie est la fille ainée de Sola Naberrie, et de son mari Darred Janren, et donc la
+                    nièce de Padmé Amidala et la cousine de Luke Skywalker et Leia Organa. À un moment indéfini,
+                    elle épousa un docteur traditionnel de Naboo avec qui elle eut trois enfants : Winama, Aimae et
+                    Irvin.
+                    Pooja Naberrie est la fille de Sola Naberrie, la nièce de Padmé Amidala. Elle est aussi la sœur
+                    de Ryoo Naberrie. La jeune Pooja aura suivi le même destin que sa tante comme étant sénatrice
+                    de Naboo.
+                    </p>
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="col-12 col-sm-4 col-lg-6 p-5 text-white">
+                <img src="img/mariage.jpg" class="img-fluid image " alt="mariage Anakin Padmé">
+
+            </div>
+            <div class="col-12 col-sm-4 col-lg-6 p-5 mt-5 text-white">
+                <h2>Son Mariage</h2>
+                <p>
+                    22 ans avant la bataille de Yavin et peu de temps après le début de la Guerre des Clones, le
+                    Chevalier Jedi Anakin Skywalker se maria en secret avec la Sénatrice Padmé Amidala avec pour
+                    seuls témoins C-3PO et R2-D2. . Elle eut 2 enfants avec lui, les jumeaux Leia et Luke, avant de
+                    mourir .
+                </p>
+            </div>
+        </div>
+    </div>
+    </div>
+    </div>
+    <!-- CONTENT START -->
+
+
+    <!-- CONTENT END -->
+
+    <?php
+include('footer.php')
+?>
