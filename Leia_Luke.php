f968f283179000f638b69385db8b15ac2d279d2a
Robin Paitry <paitrydev@gmail.com>
2019-03-22 09:19:25 +0100

    Add files via upload

---
+++ Leia_Luke.php
@@ -0,0 +1,101 @@
+<?php
+include('header.php')
+?>
+
+<body>
+
+
+
+    <!--PAGE 1 Padme-->
+    <div class="container-fluid leia">
+            <div class="container ">
+                    <div class="row">
+                        <div class=" col-3 col-sm-3 col-lg-3 ">
+                            <a href="index.php"><img src="img/senat.png" class="mt-5 pl-5" alt=""></a>
+        
+                        </div>
+                        <div class=" col-3 col-sm-3 col-lg-3 ">
+                                <a href="Anakin.php"><img src="img/anakin.png" class="mt-5 pl-5" alt=""></a>
+        
+                        </div>
+                        <div class=" col-3 col-sm-3 col-lg-3 ">
+                           <a href="Padme.php"><img src="img/padme.png" class="mt-5 pl-5" alt=""></a>
+        
+                        </div>
+                        <div class=" col-3 col-sm-3 col-lg-3 ">
+                            <a href="contact.php"><img src="img/etoile-noire.png" class="mt-5 pl-5" alt=""></a>
+                        </div>
+                    </div>
+                
+      
+                <div class="row">
+                        <div class="col-12 col-sm-12 col-lg-12 text-white mt-5">
+                            <h3>Leia Organa & Luke Skywalker</h3>
+                        </div>
+                    </div>
+            <div class="row">
+
+                <div class=" col-12 col-sm-4 col-lg-6 p-5 ">
+                    <img class="mt-2 image" src="img/leia.jpg" class="img-fluid" alt="Princesse Leia">
+                </div>
+
+                <div class="col-12 col-sm-8 col-lg-6 text-white p-5">
+                    <h2> Des parents peu communs</h2>
+                    <p class="pt-3"> Leia est la fille du légendaire Jedi Anakin Skywalker et de l'ex-reine Padmé Amidala, sénatrice de Naboo. Elle est également la sœur jumelle du chevalier Jedi Luke Skywalker. Sa mère meurt en leur donnant naissance tandis que son père bascule du côté obscur de la Force sous le nom de Dark Vador. Leia bébé est alors cachée et adoptée par la reine Breha Antilles Organa et son mari, le sénateur et prince consort Bail Organa, souverains de la planète Alderaan.<br> 
+                        <br>
+                        La nouvelle princesse Leia d'Alderaan est élue à l'âge de 18 ans, en 1 avant BY, représentante d'Alderaan au Sénat impérial, suivant ainsi la trace de son père adoptif et, sans le savoir, celle de sa mère biologique. Tout au moins avant la dissolution du Sénat impérial ordonnée par l'empereur Palpatine, alias Dark Sidious, qui confie la gouvernance des secteurs galactiques aux gouverneurs impériaux en mandat direct au début de l'épisode IV, sous le gant de fer de l'Étoile de la mort pour les maintenir dans la peur. 
+                    </p>
+                </div>  
+                <div class="col-12 col-sm-12 col-lg-12 text-white ">
+                        
+                        <p class="pt-3"> Elle est une figure importante de l'Alliance rebelle qui détruit la première puis la deuxième Étoile de la mort. Elle découvre enfin qu'elle est la sœur jumelle de Luke Skywalker et vit une histoire d'amour avec Han Solo. Trente années plus tard, elle est la générale de la Résistance qui lutte contre le Premier Ordre, et la mère de Ben Solo, l'enfant qu'elle a eu avec Han Solo et qui a basculé du côté obscur de la Force sous le nom de Kylo Ren. <br> 
+                            <br>   
+                                Ce personnage apparaît bébé dans l'épisode III lors de l'accouchement où Padmé Amidala rend son dernier souffle. Leia et Luke sont joués par le même nourrisson, qui était le fils du monteur, Roger Barton2.
+    
+                                
+                        </p>
+                    </div>         
+           </div>
+
+
+
+               
+          
+
+            <div class="row">
+               
+              <div class="col-12 col-sm-4 col-lg-6 p-5 mt-5 text-white">
+                    <h2>Sa jeunesse </h2>
+                    <p>
+                            Luke Skywalker voit le jour sur l'astéroïde de Polis Massa, peu de temps après la création de l'Empire galactique. Sa mère, Padmé Amidala, décède peu de temps après avoir choisi son nom et celui de sa sœur jumelle, Leia Organa. Au même instant, sur Mustafar, le corps mutilé de son père, Anakin Skywalker est reconstruit mécaniquement sous la supervision de l'Empereur Palpatine, prenant la forme connue de Dark Vador.
+
+                            Luke vit une enfance austère sur Tatooine. Son oncle Owen Lars et sa tante Beru Whitesun ne lui révèleront jamais ni l'existence de sa sœur ni le passé de son père, prétendant simplement que ce dernier était un « pilote de vaisseau transporteur d'épices ». Luke se voit gratifié des mêmes talents que son père : très habile en réparation de droïdes et en pilotage de vaisseaux, et doté d'une très grande présence de la force en lui, même avec une formation tardive. Il partage sa passion avec un ami, Biggs Darklighter, qui désire rejoindre l'Alliance rebelle.
+                            
+                            Ne voulant pas que Luke s'enlise dans les conflits comme son père, Owen et Beru tentent tant bien que mal de contenir ses désirs d'évasion. Owen voit d'un mauvais œil la relation que Luke entretient avec le « vieux Ben », dernier lien avec Anakin. Jusqu'à l'âge de 19 ans, Luke grandit en tant que simple garçon de ferme.
+                    </p>
+                </div>
+                <div class="col-12 col-sm-4 col-lg-6 p-5 text-white">
+                        <img src="img/luke.jpg" class="img-fluid image" alt="Luke">
+                    </div>
+            </div>
+
+
+            <div class="row">
+                <div class="col-12 col-sm-12 col-lg-12 text-white p-5">
+                    <h2>Seul au monde</h2>
+                    <p class="pt-3">Trente ans après la bataille d'Endor, Luke Skywalker est le dernier des Jedi. Il vit reclus depuis de nombreuses années sur Ahch-To, une planète océanique. Il est traqué par les membres du Premier Ordre, qui recherchent une carte permettant de le localiser et également recherché par la Résistance, menée par sa sœur jumelle Leia Organa qui a besoin de son aide. Luke s'est exilé à la suite du passage du Côté Obscur d'un de ses apprentis, son neveu Ben Solo, alors qu'il tentait de former une nouvelle génération de Jedi. Luke est finalement retrouvé par Rey, à la fin de l'épisode VII, son unique apparition dans ce film. La jeune femme lui rapporte le sabre laser d'Anakin Skywalker, qui avait été précieusement conservé par Maz Kanata.
+                    </p>
+                </div>
+
+                
+            </div>
+        </div> <!-- CONTENT START -->
+</div>
+</div>
+
+        <!-- CONTENT END -->
+
+
+<?php
+include('footer.php')
+?>
