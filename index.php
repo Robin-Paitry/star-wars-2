d0df94241430ba08ccf79924accbb3e488421a14
Robin Paitry <paitrydev@gmail.com>
2019-03-22 09:17:43 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+include('header.php')
+?>
+
+<body>
+
+    <!-- CONTENT START -->
+    <header class="fondaccueil">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+
+                    <h1 class="mt-5"><span class="text-white titre1">STAR WAR</span><span class="rior">RIOR</span> <span class="text-white titre1-s">S</span></h1>
+                    <div class="row">
+                        <div class="col-4">
+
+                            <div class="row">
+                                <div class="col-3">
+
+                                   <a href="Padme.php"><img src="img/padme.png" class="mb-5" alt=""></a> 
+                                   <a href="Anakin.php"><img src="img/anakin.png" class="mb-5" alt=""></a>
+                                    <a href="leia_luke.php"><img src="img/luke-leia.png" class="mb-5" alt=""></a>
+                                    <a href="contact.php"><img src="img/etoile-noire.png" class="mb-5" alt=""></a>
+
+
+                                </div>
+
+
+                                <div class="col-9">
+                                 <a href="Padme.html"> <p class="text-white nom-perso" align="left">Padmée Amidala</p>
+                                 <a href="Anakin.html"> <p class="text-white nom-perso-2" align="left">Anakin Skywalker</p></a>
+                                    <a href="Leia_Luke.html"><p class="text-white nom-perso-2" align="left">Luke Skywalker<br> & Leia Organa
+                                    </p></a>
+                                   <a href="contact.html"> <p class="text-white nom-perso" align="left">Contact
+                                    </p></a>
+
+                                </div>
+
+
+
+
+
+
+
+
+                            </div>
+
+                        </div>
+
+                        <div class="col-4 text-intro">
+                            <p class="text-white">Star Wars ne parle que de ça. La famille, avec tous ces secrets,
+                                petits ou grands, la lignée des Skywalker, perdue puis retrouvée.</p>
+                            <p class="text-white">De «Je suis ton père» à «Tu es ma sœur», la saga intergalactique de
+                                George Lucas noue et dénoue les liens familiaux dans la plus pure tradition dramatique
+                                de génération en génération.</p>
+
+                            <p class="text-white"> Star Wars, en fait, c'est le conte de fée galactique d'une famille
+                                déchue, dispersée, oubliée, qui trouvera la force résiliente de se reformer, pour mieux
+                                pardonner aux anciens. Car comme il est dit dans la Bible: «Quand les parents mangent
+                                des raisins trop verts, ce sont les enfants qui en ont les dents agacées»</p>
+                            <P class="text-white">Ce site présente une biographie relativement simple des différents
+                                membres de la famille Skywalker.</P>
+                                <P class="text-white ml-1 pl-5">BIENVENUE SUR NOTRE SITH!</P>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </header>
+
+    <!-- CONTENT END -->
+<?php
+include('footer.php')
+?>
