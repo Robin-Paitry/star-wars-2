6f3e84f76290725d12b6b5a2576d61c734a68807
Robin Paitry <paitrydev@gmail.com>
2019-03-22 09:18:25 +0100

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,84 @@
+<?php
+include('header.php')
+?>
+
+
+<body>
+
+
+    <!-- CONTENT START -->
+    <footer class="">
+        <div class=" container-fluid fondcontact">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+
+                        <h1 class="mt-5 text-white titre-centre">CONTACT</h1>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-md-6 ">
+                        <form>
+                            <div class="form-group ">
+                                <label class="text-white" for="exampleFormControlInput1">PSEUDO</label>
+                                <input type="email" class="form-control text-white" id="exampleFormControlInput1" style="background-color:transparent; border-color: white; border-width: medium; border-style: solid">
+                            </div>
+                            <div class="form-group">
+                                <label class="text-white" for="exampleFormControlInput1"> EMAIL</label>
+                                <input type="email" class=" form-control text-white" id="exampleFormControlInput1"
+                                    style="background-color:transparent; border-color: white; border-width: medium; border-style: solid">
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-md-6 p-4">
+                        <p class="text-white text-contact">10, Jabba street<br>
+                            Mos Espa Tatouine<br>
+                            <p class="text-white text-contact">Dark.site@coteobscur.com</p>
+
+                        </p>
+
+                    </div>
+                    <div class="col-12  ">
+                        <div class="form-group ">
+                            <label class="text-white" for="exampleFormControlTextarea1">MESSAGE</label>
+                            <textarea class=" form-control text-white " id="exampleFormControlTextarea1" rows="7" style="background-color:transparent; border-color: white; border-width: medium; border-style: solid"></textarea>
+                            <button type="button" class="btn btn mt-5" style="background-color:#403f3e; color:white; width: 300px">MESSAGE</button>
+                        </div>
+                    </div>
+
+                    <div class="col-12 d-flex justify-content-between">
+                        <a href="index.php"> <img src="img/senat.png"></a>
+                        <a href="Padme.php"> <img src="img/padme.png"></a>
+                        <a href="Anakin.php"> <img src="img/anakin.png"></a>
+
+                        <a href="Leia_Luke.php"> <img src="img/luke-leia.png"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </footer>
+    <!-- CONTENT END -->
+
+    <!-- Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+    <!-- Custom JS -->
+    <script src="js/main.js"></script>
+
+</body>
+
+
+<?php
+include('footer.php')
+?>
