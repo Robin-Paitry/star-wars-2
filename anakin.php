35baa4f3311d8bbd2e7d66c608124a23fec273ba
Robin Paitry <paitrydev@gmail.com>
2019-03-22 09:19:04 +0100

    Add files via upload

---
+++ anakin.php
@@ -0,0 +1,165 @@
+<?php
+include('header.php')
+?>
+<body>
+
+
+
+<!--PAGE 1 Padme-->
+<div class="container-fluid anakin">
+
+        <div class="container ">
+                <div class="row">
+                    <div class=" col-3 col-sm-3 col-lg-3 ">
+                        <a href="index.php"><img src="img/senat.png" class="mt-5 pl-5" alt=""></a>
+    
+                    </div>
+                    <div class=" col-3 col-sm-3 col-lg-3 ">
+                            <a href="Padme.php"><img src="img/padme.png" class="mt-5 pl-5" alt=""></a>
+    
+                    </div>
+                    <div class=" col-3 col-sm-3 col-lg-3 ">
+                       <a href="Leia_Luke.php"><img src="img/luke-leia.png" class="mt-5 pl-5" alt=""></a>
+    
+                    </div>
+                    <div class=" col-3 col-sm-3 col-lg-3 ">
+                        <a href="contact.php"><img src="img/etoile-noire.png" class="mt-5 pl-5" alt=""></a>
+                    </div>
+                </div>
+    <div  class="container">
+            <div class="row">
+                    <div class="col-12 col-sm-12 col-lg-12 text-white mt-5">
+                        <h3>Anakin Skywalker</h3>
+                    </div>
+                </div>
+        <div class="row">
+
+            <div class=" col-12 col-sm-4 col-lg-6 p-5 ">
+                <img class=" image mt-2" src="img/Anakin1.png"  alt="Anakin">
+                
+            </div>
+
+            <div class="col-12 col-sm-8 col-lg-6 text-white p-5">
+                <h2>Un destin ténébreux </h2>
+                <p class="pt-3"> Anakin Skywalker, ou Dark Vador (en anglais : Darth Vader), est un personnage de
+                    fiction, successivement chevalier Jedi et seigneur Sith, et le personnage central des deux
+                    premières trilogies de la saga cinématographique Star Wars conçue par George Lucas. Dans La
+                    Menace Fantôme, Anakin est un enfant de petite taille aux cheveux courts et blonds (coupe au
+                    bol) et aux yeux bleus, pilote de module de courses. Il est esclave sur Tatooine. À la fin du
+                    film, il porte la coupe de cheveux (avec la tresse signifiant qu’il est apprenti) et les
+                    vêtements des Padawan.<br> Dans L'Attaque des clones, il est devenu adulte et a donc
+                    considérablement grandi. Il mesure dans les 1,80 m et arbore toujours la coupe de cheveux des
+                    Padawans. Lors du duel final contre le comte Dooku, il se fait trancher l'avant-bras droit et
+                    est contraint de porter une prothèse mécanique.
+                </p>
+            </div>
+        </div>
+
+<div class="row">
+    <div class="col-12 col-sm-12 col-lg-12 text-white">
+                <p> Dans La Revanche des Sith, il a
+            considérablement laissé pousser ses cheveux et une cicatrice est présente sur sa tempe droite.
+            Durant le massacre du Conseil Séparatiste, ses yeux sont jaunes et injectés de sang, ce qui
+            montre une forte emprise du Côté Obscur. Lorsqu'il perd son duel contre son ancien maître Obi
+            wan Kenobi , à la fin de l'épisode III, il perd en même temps le dernier bras qui lui reste,
+            ainsi que ses deux jambes. La colère et la rage déforment son visage avant que son corps ne
+            soit presque entièrement brûlé par de la lave en fusion.<br> Son corps sera désormais couvert
+            de cicatrices et de traces de brûlures et pour survivre, Anakin sera enfermé dans une sombre
+            armure de survie qu'il occupera jusqu'à la fin de sa vie. À la fin du Retour du Jedi, lorsque
+            son fils Luke lui retire le masque de Dark Vador, le visage d'Anakin est défiguré par de
+            longues balafres sur ses joues et sur le haut de son crâne totalement chauve.</p>
+</div>
+
+
+        <div class="row">
+            <div class="col-12 col-sm-12 col-lg-12 text-white p-5">
+                <h2>Un objet si spécial</h2>
+                <p class="pt-3"> Tout au long de la saga, on remarque qu'un objet particulier d'Anakin Skywalker se
+                    transmet sur plusieurs générations : son sabre laser.
+
+                    Au début de la Guerre des clones, Anakin se fabrique un nouveau sabre laser à lame bleue pour
+                    remplacer celui qu'il avait perdu sur Géonosis dans l'épisode II. Durant la Guerre des Clones,
+                    il utilisera très souvent son sabre laser afin de combattre les droïdes de combat de la
+                    Fédération du Commerce et pour combattre les Seigneurs noirs des Sith, tels que le Comte Dooku,
+                    Asajj Ventress ou encore Savage Opress.
+                    <br>
+                    <br> Au début de l'épisode III, le Jedi utilise une nouvelle fois son arme afin de terrasser le
+                    Comte Dooku et pour combattre les droïdes de combat du Général Grievous. Plus tard dans
+                    l'épisode, Skywalker, devenu Dark Vador, utilise à nouveau son sabre laser pour exterminer les
+                    jeunes novices Jedi ainsi que la totalité du Conseil Séparatiste. C'est sur la planète
+                    volcanique Mustafar qu'Anakin utilisera son sabre laser pour la dernière fois. Après un très
+                    long et fatigant duel contre son ancien maître Obi-Wan Kenobi, Skywalker perd son sabre laser
+                    ainsi que trois de ses membres, qui lui sont tranchés par Obi-Wan. Kenobi récupère le sabre
+                    laser de l'apprenti Sith et le conservera précieusement dans son exil sur Tatooine durant dix
+                    neuf ans afin qu'il puisse le transmettre un jour au fils de celui-ci, Luke, lorsqu'il sera en
+                    âge de combattre.
+                    <br>
+
+                    <br>Trois ans plus tard, dans l'épisode V, Luke utilise son sabre laser sur la planète Hoth
+                    afin de terrasser un Wampa des Glaces. Il l'utilisera également quelques jours plus tard, lors
+                    de la Bataille de Hoth. Durant son entraînement de Jedi en compagnie de Yoda sur la planète
+                    Dagobah , Luke apprend le maniement du sabre laser. À la fin de l'épisode, Luke utilise son
+                    arme afin de combattre Dark Vador (son propre père) dans un duel au sabre laser sur Bespin.
+                    Après un long combat, Vador est victorieux et Luke perd son sabre laser ainsi que sa main
+                    droite. L'arme tombe dans un conduit d'aération de la Cité des Nuages et disparaît.
+                    <br>
+                    Trente-et-un ans plus tard, dans l'épisode VII, le sabre laser d'Anakin Skywalker est retrouvé
+                    par Rey sur la planète Takodana, où Maz Kanata conserve précieusement l'arme qu'elle cache dans
+                    un coffre depuis des années. Maz Kanata explique brièvement l'histoire du sabre laser à Rey,
+                    puis finit par le donner à Finn. Plus tard dans l'épisode, c'est successivement Finn et Rey qui
+                    utilisent le sabre laser d'Anakin afin de combattre Kylo Ren (qui est en réalité le petit fils
+                    de Dark Vador/Anakin Skywalker). À la fin du film, Rey retrouve Luke Skywalker sur une planète
+                    océanique et lui tend alors le sabre laser qui a jadis appartenu à son père Anakin.<br>
+                    <br>
+                    Dans l'épisode VIII, Rey redonne son sabre à Luke, mais celui ci préfère le jeter au bord de la
+                    falaise. L'arme est ramassée par Rey qui l'utilise comme son propre sabre laser durant sa
+                    courte formation auprès de Luke. En quittant Ach-To, la jeune femme se laisse capturer par les
+                    membres du Premier Ordre car elle souhaite aider Kylo Ren à affronter son côté obscur. Rey est
+                    amenée devant le suprême leader Snoke, qui ordonne à Kylo de la tuer pour achever sa formation.
+                    Kylo Ren désobéit, et utilise le sabre laser de son grand père à distance pour terrasser son
+                    maître en le tranchant en deux. Puis, Kylo et Rey utilisent leur sabres respectifs pour
+                    combattre et tuer les gardes prétoriens présents dans la salle.<br> En voulant récupérer son
+                    sabre des mains de Kylo, Rey utilise la Force, mais le jeune homme fait le même geste en
+                    retenant le sabre de son côté, et l'arme lévite dans les airs pendant quelques instants,
+                    attirée par les deux combattants. La force d'attraction est tellement forte que la précieuse
+                    relique finit par se briser en deux morceaux au moment où le pont du vaisseau explose. Rey
+                    s'éveille et abandonne Kylo sur place en emportant avec elle les restes du sabre laser d'Anakin
+                    Skywalker. C'est toutefois ce sabre que la projection mentale de Luke utilise, lors du combat
+                    final contre Kylo Ren, sur Crayt.
+                </p>
+            </div>
+
+
+
+        </div>
+        <div class="row">
+            <div class="col-12 col-sm-4 col-lg-6 p-5 text-white">
+                <img src="img/Dark-vador.png" class="img-fluid image" alt="Dark-Vador">
+            </div>
+            <div class="col-12 col-sm-4 col-lg-6 p-5 mt-5 text-white">
+                <h2>THE DARK SIDE</h2>
+                <p>
+                    22 ans avant la bataille de Yavin et peu de temps après le début de la Guerre des Clones, le
+                    Chevalier Jedi Anakin Skywalker se maria en secret avec la Sénatrice Padmé Amidala avec pour
+                    seuls témoins C-3PO et R2-D2. . Elle eut 2 enfants avec lui, les jumeaux Leia et Luke, avant de
+                    mourir .
+                </p>
+                <h2>LA PROPHÉTIE L'ÉLU </h2>
+                <p class="pt-3">
+                    Cette prophétie, dévoilée dans l'épisode I, nous raconte qu'un Élu viendra rétablir l'équilibre
+                    dans la Force. Suite à un test pratiqué par Qui-Gon, le maître Jedi d'Obi-Wan, Anakin se révèle
+                    être le potentiel Élu décrit par la prophétie. En tuant Palpatine dans l'épisode VI, Anakin
+                    prouve qu'il était bel et bien l'élu, malgré ses erreurs passées. </p>
+            </div>
+           
+        </div>
+    </div>
+</div>
+</div> <!-- CONTENT START -->
+
+
+<!-- CONTENT END -->
+    <!-- CONTENT END -->
+<?php
+include('footer.php')
+?>
